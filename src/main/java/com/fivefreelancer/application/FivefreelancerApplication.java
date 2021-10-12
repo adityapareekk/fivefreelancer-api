e17493842e84e90683d317d5c0899fd4613b9274
Aditya <adityapareek898@gmail.com>
2021-10-12 11:34:22 +0530

    fivefreelancer project update

---
+++ src/main/java/com/fivefreelancer/application/FivefreelancerApplication.java
@@ -0,0 +1,13 @@
+package com.fivefreelancer.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivefreelancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivefreelancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fivefreelancer/application/FivefreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivefreelancer.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivefreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
